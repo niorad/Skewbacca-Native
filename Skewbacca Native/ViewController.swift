d8e527d33f4b9023c97899c53b89731489451c62
niorad <mail@niorad.com>
2019-01-01 22:37:59 +0100

    Initial Commit

---
+++ Skewbacca Native/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Skewbacca Native
+//
+//  Created by Antonio Radovcic on 01.01.19.
+//  Copyright © 2019 niorad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
